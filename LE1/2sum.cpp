d422f207ca5523964c1fb743f1982ac8152d496e
231B322 <231B322@juet.co.in>
2025-07-22 15:48:39 +0530

    update

--- LE1/2sum.cpp
+++ LE1/2sum.cpp
@@ -1,16 +1,15 @@
 #include<iostream>
 using namespace std;
 
-func 2sum(int a, int b){
+int sum(int a, int b){
    return a+b;
 }
 
 int main(){
     int a;
     int b;
-    printf("Sum of a & b is %d", 2sum(a,b));
+    printf("Enter value of a & b \n");
+    cin>>a;
+    cin>>b;
+    printf("Sum of a & b is %d", sum(a,b));
 }
-
-
-
-
